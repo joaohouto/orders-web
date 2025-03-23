237d0a72aa40ce0978819e96c10f2380d7a1296a
João Couto <joaocouto.jar@gmail.com>
2025-03-23 18:58:32 -0400

    reboot

--- src/utils/formatNumber.js
+++
@@ -1,9 +0,0 @@
-const formatNumber = (number) => {
-  return parseFloat(number)
-    .toFixed(2)
-    .toString()
-    .replace(".", ",")
-    .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-};
-
-export default formatNumber;
