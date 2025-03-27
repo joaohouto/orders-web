5bda146f9d8d340a95235c9de0469f5c76df9abf
João Couto <joaocouto.jar@gmail.com>
2025-03-26 20:46:46 -0400

    GitHub Actions

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,13 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { StoreIcon } from "lucide-react";
 
 export function AppPage() {
   return (
     <div className="min-h-screen bg-muted">
-      <h1>Suas lojas</h1>
+      <div className="w-full md:max-w-[400px] mx-auto my-auto flex flex-col gap-4 px-4 py-8">
+        <h1 className="font-semibold tracking-tight text-2xl">Suas lojas</h1>
 
-      <div></div>
+        <div className="flex flex-col gap-2">
+          <Card>
+            <CardHeader>
+              <StoreIcon />
+              <CardTitle>Direito Aquidauana</CardTitle>
+            </CardHeader>
+          </Card>
+        </div>
 
-      <Button>Criar nova loja</Button>
+        <Button>Criar nova loja</Button>
+      </div>
     </div>
   );
 }
